a68e0a2c4504496a14bd0ec332b831760ac5ff1c
ppanko <pavelppanko@gmail.com>
2021-01-09 18:01:15 -0500

    First commit

---
+++ init.el
@@ -0,0 +1,29 @@
+;; Configure package.el to include MELPA.
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(package-initialize)
+
+;; Ensure that use-package is installed.
+;;
+;; If use-package isn't already installed, it's extremely likely that this is a
+;; fresh installation! So we'll want to update the package repository and
+;; install use-package before loading the literate configuration.
+(when (not (package-installed-p 'use-package))
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(org-babel-load-file "~/.emacs.d/config.org")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ess-rdired-auto-update-interval 60)
+ '(package-selected-packages
+   '(synosaurus ivy-ric ivy wgrep use-package transpose-frame telephone-line smex restart-emacs projectile poly-R org-tree-slide org-bullets multiple-cursors magit google-this flycheck ess elpy base16-theme autopair auto-package-update auto-compile ace-window)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
