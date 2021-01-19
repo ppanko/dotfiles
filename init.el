f090ad7538773cba4741e6477196f0b678a383a4
Pavel Panko <ppanko@ida.org>
2021-01-19 12:32:36 -0500

    Fixing R project creation function

--- init.el
+++ init.el
@@ -19,7 +19,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(shell-pop ivy-rich swiper-helm counsel synosaurus ivy-ric ivy wgrep use-package transpose-frame telephone-line smex restart-emacs projectile poly-R org-tree-slide org-bullets multiple-cursors magit google-this flycheck ess elpy base16-theme autopair auto-package-update auto-compile ace-window)))
+   (quote
+    (shell-pop ivy-rich swiper-helm counsel synosaurus ivy-ric ivy wgrep use-package transpose-frame telephone-line smex restart-emacs projectile poly-R org-tree-slide org-bullets multiple-cursors magit google-this flycheck ess elpy base16-theme autopair auto-package-update auto-compile ace-window))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
