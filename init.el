0cdf189a92a29a063a744773f1810ae6a591e5b5
ppanko <pavelppanko@gmail.com>
2023-04-20 14:43:25 -0400

    a

--- init.el
+++ init.el
@@ -18,13 +18,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(org-agenda-files nil)
  '(package-selected-packages
-   '(magit hide-mode-line doom-modeline shrink-path f workgroups2 wgrep visual-fill-column use-package undo-tree transpose-frame telephone-line synosaurus super-save smex smartparens shell-pop restart-emacs rainbow-mode projectile poly-R org-tree-slide org-present org-bullets multiple-cursors ivy-rich google-this git-gutter-fringe+ flycheck ess elpy ein doom-themes counsel base16-theme auto-package-update auto-compile async all-the-icons-ivy all-the-icons-dired ace-window)))
+   '(org-ref ivy-bibtex org-roam-bibtex org-agenda workgroups2 wgrep vlf visual-fill-column visual-fill use-package undo-tree transpose-frame telephone-line synosaurus super-save smex smartparens shell-pop restart-emacs rainbow-mode projectile poly-R org-tree-slide org-superstar org-roam org-present multiple-cursors magit ivy-rich hlinum hide-mode-line helm-core google-this git-gutter-fringe git-gutter-fringe+ flycheck ess-view-data elpy doom-themes doom-modeline counsel base16-theme auto-package-update auto-compile all-the-icons-ivy all-the-icons-dired ace-window)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-level-1 ((t (:inherit outline-1 :height 1.5))))
- '(org-level-2 ((t (:inherit outline-2 :height 1.2))))
- '(org-level-3 ((t (:inherit outline-3 :height 1.1)))))
+ )
