195f6b9f96d6ac6291fc81c4cc714e0c955ea383
Pavel Panko <ppanko@ida.org>
2025-11-14 09:37:38 -0500

    Improve DT viewer capability

--- init.el
+++ init.el
@@ -13,3 +13,16 @@
   (package-install 'use-package))
 
 (org-babel-load-file "~/.emacs.d/config.org")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(jupyter workgroups2 visual-fill-column vertico use-package unicode-fonts undo-tree transpose-frame synosaurus super-save stan-mode smex smartparens ripgrep rg restart-emacs request rainbow-mode projectile poly-R org-present nerd-icons multiple-cursors marginalia magit ivy-rich hide-mode-line gptel google-this gnu-elpa-keyring-update git-gutter-fringe+ flycheck ess elpy doom-themes doom-modeline counsel-tramp citar-org-roam auto-package-update auto-compile async all-the-icons-ivy all-the-icons-dired ace-window)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
