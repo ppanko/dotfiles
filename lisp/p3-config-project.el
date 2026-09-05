ea6303655b60f8df6c47c6ef033bcdcfa75c0246
Pavel Panko <18711790+ppanko@users.noreply.github.com>
2026-09-05 12:35:07 -0400

    Add native project configuration owner

---
+++ lisp/p3-config-project.el
@@ -0,0 +1,10 @@
+;;; p3-config-project.el --- Native project configuration -*- lexical-binding: t; -*-
+
+(require 'project)
+
+(global-set-key (kbd "C-c p") project-prefix-map)
+(global-set-key (kbd "s-p") project-prefix-map)
+
+(provide 'p3-config-project)
+
+;;; p3-config-project.el ends here
