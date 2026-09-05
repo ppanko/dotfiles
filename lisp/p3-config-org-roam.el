77674f820ab5eb1c738c5dab1014e57ecfb32d93
Pavel Panko <18711790+ppanko@users.noreply.github.com>
2026-09-05 00:48:03 -0400

    Extract Org-roam configuration

---
+++ lisp/p3-config-org-roam.el
@@ -0,0 +1,57 @@
+;;; p3-config-org-roam.el --- Org-roam configuration -*- lexical-binding: t; -*-
+
+(require 'use-package)
+(require 'p3-config-loader)
+
+(defvar org-roam-node-display-template)
+
+(declare-function org-roam-db-autosync-mode "org-roam-db" (&optional arg))
+
+(p3/config-load-module 'p3-org-roam)
+
+(use-package org-roam
+  :hook
+  (after-init . org-roam-mode)
+  :custom
+  (org-roam-database-connector 'sqlite-builtin)
+  (org-roam-directory "~/org/notes/roam/")
+  (org-roam-completion-everywhere t)
+  (org-roam-completion-system 'default)
+  (org-roam-capture-templates
+   '(("d" "default" plain "%?"
+      :if-new
+      (file+head "%<%Y%m%d%H%M%S>-${slug}.org"
+                 "#+title: ${title}\n#+category:${title}\n#+created: %U\n#+last_modified: %U\n")
+      :unnarrowed t)
+     ("n" "literature note" plain "* Heading\n %?"
+      :target
+      (file+head
+       "%(expand-file-name (or citar-org-roam-subdir \"\") org-roam-directory)/${citar-citekey}.org"
+       "#+title: ${citar-citekey} (${citar-date}). ${note-title}.\n#+created: %U\n#+last_modified: %U\n\n")
+      :unnarrowed t)))
+  (org-roam-dailies-directory "journal/")
+  (org-roam-dailies-capture-templates
+   '(("d" "default" entry "* %<%I:%M %p>: %?"
+      :target
+      (file+head "%<%Y-%m-%d>.org"
+                 "#+title: %<%Y-%m-%d %a>\n#+created: %U\n#+last_modified: %U\n"))))
+  :bind (("C-c n l" . org-roam-buffer-toggle)
+         ("C-c n f" . org-roam-node-find)
+         ("C-c n g" . org-roam-graph)
+         ("C-c n i" . org-roam-node-insert)
+         ("C-c n c" . org-roam-capture)
+         ("C-c n n" . org-roam-node-insert-immediate-with-tag)
+         ("C-c n s" . org-roam-rg-search)
+         ("C-c n d" . org-roam-dailies-goto-today)
+         ("C-c n t" . org-roam-dailies-capture-today)
+         ("C-c n C-t" . org-roam-tag-add)
+         ("C-c n a" . p3/org-roam-get-agenda))
+  :config
+  (setq org-roam-node-display-template
+        (concat "${title:*} "
+                (propertize "${tags:10}" 'face 'org-tag)))
+  (org-roam-db-autosync-mode))
+
+(provide 'p3-config-org-roam)
+
+;;; p3-config-org-roam.el ends here
