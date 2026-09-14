c5e5c83946097dab902a1980838d37787a18e153
Pavel Panko <18711790+ppanko@users.noreply.github.com>
2026-09-14 10:16:19 -0400

    feat: load screen recording behavior

--- lisp/p3-config-base.el
+++ lisp/p3-config-base.el
@@ -6,6 +6,7 @@
 (require 'p3-config-loader)
 (p3/config-load-module 'p3-commands)
 (p3/config-load-module 'p3-core)
+(p3/config-load-module 'p3-screen-record)
 
 (defvar dashboard-startup-banner)
 (defvar dashboard-center-content)
@@ -27,7 +28,7 @@
 (declare-function dashboard-setup-startup-hook "dashboard" ())
 (declare-function which-key-mode "which-key" (&optional arg))
 (declare-function which-key-add-key-based-replacements "which-key" (&rest replacements))
-(declare-function dired-async-mode "dired-async" (&optional arg))
+(declare-function dired-async-mode "async" (&optional arg))
 (declare-function async-bytecomp-package-mode "async-bytecomp" (&optional arg))
 (declare-function package-refresh-contents "package" (&optional async))
 (declare-function package-list-packages "package" (&optional no-fetch))
