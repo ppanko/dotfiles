efee3d3a7ca083ca8b15a700ef55b66f4081befa
Pavel Panko <18711790+ppanko@users.noreply.github.com>
2026-09-16 23:17:51 -0400

    test: wait for Eat fixture to finish

--- test/p3-terminal-test-support.el
+++ test/p3-terminal-test-support.el
@@ -33,65 +33,62 @@
          (command (mapconcat #'shell-quote-argument
                              (list python fixture (number-to-string exit-code))
                              " "))
-         timer
-         timeout-timer
+         (deadline (+ (float-time) 5.0))
          observed-terminal
          observed-raw-escape
-         input-sent)
-    (setq timer
-          (run-at-time
-           0.05 0.05
-           (lambda ()
-             (when (buffer-live-p buffer)
-               (with-current-buffer buffer
-                 (when-let ((proc (eshell-head-process)))
-                   (save-excursion
-                     (goto-char (point-min))
-                     (when (search-forward "__P3_CURSOR__" nil t)
-                       (setq observed-terminal t)
-                       (goto-char (point-min))
-                       (setq observed-raw-escape
-                             (search-forward "\033[" nil t))
-                       (process-send-string proc "x")
-                       (setq input-sent t)
-                       (cancel-timer timer)))))))))
-    (setq timeout-timer
-          (run-at-time
-           5 nil
-           (lambda ()
-             (when (buffer-live-p buffer)
-               (with-current-buffer buffer
-                 (when-let ((proc (eshell-head-process)))
-                   (delete-process proc)))))))
-    (unwind-protect
-        (progn
-          (with-current-buffer buffer
-            (goto-char (point-max))
-            (insert command)
-            (eshell-send-input))
-          (with-current-buffer buffer
-            (let (tty size input)
-              (save-excursion
+         input-sent
+         finished)
+    (with-current-buffer buffer
+      (goto-char (point-max))
+      (insert command)
+      (eshell-send-input))
+    (while (and (not finished) (< (float-time) deadline))
+      (accept-process-output nil 0.05)
+      (when (buffer-live-p buffer)
+        (with-current-buffer buffer
+          (unless input-sent
+            (save-excursion
+              (goto-char (point-min))
+              (when (search-forward "__P3_CURSOR__" nil t)
+                (setq observed-terminal t)
                 (goto-char (point-min))
-                (when (re-search-forward "__P3_TTY__\\([01]:[01]\\)" nil t)
-                  (setq tty (match-string-no-properties 1)))
-                (goto-char (point-min))
-                (when (re-search-forward
-                       "__P3_SIZE__\\([0-9]+\\):\\([0-9]+\\)" nil t)
-                  (setq size (cons (string-to-number
-                                    (match-string-no-properties 1))
-                                   (string-to-number
-                                    (match-string-no-properties 2)))))
-                (goto-char (point-min))
-                (when (re-search-forward "__P3_INPUT__\\([0-9a-f]+\\)" nil t)
-                  (setq input (match-string-no-properties 1))))
-              (list :terminal observed-terminal
-                    :raw-escape observed-raw-escape
-                    :input-sent input-sent
-                    :tty tty
-                    :size size
-                    :input input))))
-      (when (timerp timer) (cancel-timer timer))
-      (when (timerp timeout-timer) (cancel-timer timeout-timer)))))
+                (setq observed-raw-escape
+                      (search-forward "\033[" nil t))
+                (when-let ((proc (eshell-head-process)))
+                  (process-send-string proc "x")
+                  (setq input-sent t)))))
+          (save-excursion
+            (goto-char (point-min))
+            (setq finished
+                  (and input-sent
+                       (re-search-forward "__P3_INPUT__[0-9a-f]+" nil t)))))))
+    (unless finished
+      (when (buffer-live-p buffer)
+        (with-current-buffer buffer
+          (when-let ((proc (eshell-head-process)))
+            (delete-process proc)))))
+    (with-current-buffer buffer
+      (let (tty size input)
+        (save-excursion
+          (goto-char (point-min))
+          (when (re-search-forward "__P3_TTY__\\([01]:[01]\\)" nil t)
+            (setq tty (match-string-no-properties 1)))
+          (goto-char (point-min))
+          (when (re-search-forward
+                 "__P3_SIZE__\\([0-9]+\\):\\([0-9]+\\)" nil t)
+            (setq size (cons (string-to-number
+                              (match-string-no-properties 1))
+                             (string-to-number
+                              (match-string-no-properties 2)))))
+          (goto-char (point-min))
+          (when (re-search-forward "__P3_INPUT__\\([0-9a-f]+\\)" nil t)
+            (setq input (match-string-no-properties 1))))
+        (list :terminal observed-terminal
+              :raw-escape observed-raw-escape
+              :input-sent input-sent
+              :tty tty
+              :size size
+              :input input
+              :finished finished)))))
 
 (provide 'p3-terminal-test-support)
