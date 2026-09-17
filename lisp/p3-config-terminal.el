9c6c840023d95b19551d272ec7f00f4041374760
Pavel Panko <18711790+ppanko@users.noreply.github.com>
2026-09-17 14:03:57 -0400

    fix: use Eat exit hook for project visual lifecycle

--- lisp/p3-config-terminal.el
+++ lisp/p3-config-terminal.el
@@ -7,7 +7,7 @@
 
 (declare-function p3/windows-configure-shell "p3-platform" ())
 (declare-function p3/project-shell "p3-terminal" (&optional new-session))
-(declare-function p3/project-shell-eat-visual-buffer-setup
+(declare-function p3/project-shell-eat-visual-buffer-exit
                   "p3-terminal" (process))
 (declare-function eshell-syntax-highlighting-global-mode
                   "eshell-syntax-highlighting" (&optional arg))
@@ -27,7 +27,7 @@
 (use-package eat
   :after eshell
   :config
-  (add-hook 'eat-exec-hook #'p3/project-shell-eat-visual-buffer-setup)
+  (add-hook 'eat-exit-hook #'p3/project-shell-eat-visual-buffer-exit)
   (eat-eshell-visual-command-mode 1))
 
 (global-set-key (kbd "C-x C-u") #'p3/project-shell)
