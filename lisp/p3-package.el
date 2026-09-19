7784b3a583cb5e55e8c757a0978916279ecd2ec5
Pavel Panko <18711790+ppanko@users.noreply.github.com>
2026-09-19 18:54:07 +0000

    Restore use-package ensure return contract

--- lisp/p3-package.el
+++ lisp/p3-package.el
@@ -192,8 +192,8 @@ When DEFER-ACTIVATION is non-nil, leave final activation to the caller."
           (error
            (error "Package bootstrap failed for %s: %s"
                   package
-                  (error-message-string err))))
-  t))))
+                  (error-message-string err))))))))
+  t)
 
 (provide 'p3-package)
 
